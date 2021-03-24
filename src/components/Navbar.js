323cfb9f9f27f35024b52d643c8ac634a798d8d6
Miguel Oliveira <miguel_250597@hotmail.com>
2021-03-24 21:53:35 +0000

    add burger dropdown menu for mobile width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { IndexContext } from './IndexContext'
 import menuItems from '../routes/MenuItems'
@@ -8,18 +8,25 @@ import './Navbar.css'
 export default function Navbar() {
 
 	const { selectedIndex } = useContext(IndexContext)
+	const [burgerExpanded, setBurgerExpanded] = useState(false)
 
 	return (
 		<nav className='navbar'>
 			<h1>Website</h1>
-			<ul className='nav-links'>
+			<ul className='nav-links' style={{display: burgerExpanded ? 'flex' : null}}>
 				{menuItems.map(({ title, path }, index) => 
 					<Link key={index} to={path} className='link' 
-					style={index === selectedIndex ? {'borderBottom': '1px #333 solid'} : null}>
+					style={{borderBottom: index === selectedIndex && '1px #333 solid'}}>
 						<li>{title}</li>
 					</Link>
 				)}
 			</ul>
+
+			<div class='nav-burger' onClick={() => setBurgerExpanded(!burgerExpanded)}>
+				<div className='nav-burger-line'></div>
+				<div className='nav-burger-line'></div>
+				<div className='nav-burger-line'></div>
+			</div>
 		</nav>
 	)
 }
